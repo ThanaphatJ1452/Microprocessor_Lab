f2e9d353998b59ff7afd79472d2b06f17e16c535
Emma Paris <eparis@atlassian.com>
2020-02-16 14:25:08 +0700

    using shift register

--- Week3/Week3_Task3/src/main.cpp
+++ Week3/Week3_Task3/src/main.cpp
@@ -1,71 +1,60 @@
 #include <Arduino.h>
 
-int clk_pin= 13;//SHCP 2
-int latch_pin= 10;//STCP 3
-int data_pin= 11;//DS 4
 unsigned short count = 0;
-int key1;
-int key2;
-void setLed(uint8_t);
+unsigned short key1;
+unsigned short key2;
+
+#define a  0b00000001
+#define b  0b00000010
+#define c  0b00000100
+#define d  0b00001000
+#define e  0b00010000
+#define f  0b00100000
+#define g  0b01000000
+#define dp 0b10000000
+
+#define num0 a | b | c | d | e | f
+#define num1 b | c
+#define num2 a | b | d | e | g
+#define num3 a | b | c | d | g
+#define num4 b | c | f | g
+#define num5 a | c | d | f | g
+#define num6 a | c | d | e | f | g
+#define num7 a | b | c | f
+#define num8 a | b | c | d | e | f | g 
+#define num9 a | b | c | d | f | g
+
+unsigned short charecter[10] = {num0, num1, num2, num3, num4, num5, num6, num7, num8, num9};
 void led_Sequential(unsigned short);
-uint8_t number[10][8] = {
-                          {0, 1, 1 ,1, 1, 1, 1, 0}, //0
-                          {0, 1, 1, 0, 0, 0, 0, 0}, //1
-                          {1, 0, 1, 1, 0, 1, 1, 0}, //2  {g, c, b, a, f, e, d}
-                          {1, 1, 1, 1, 0, 0, 1, 0}, //3
-                          {1, 1, 1, 0, 1, 0, 0, 0}, //4
-                          {1, 1, 0, 1, 1, 0, 1, 0}, //5
-                          {1, 1, 0, 1, 1, 1, 1, 0}, //6
-                          {0, 1, 1, 1, 0, 0, 0, 0}, //7
-                          {1, 1, 1, 1, 1, 1, 1, 0}, //8
-                          {1, 1, 1, 1, 1, 0, 1, 0}  //9
-};
-int number_1[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F}; 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(clk_pin, OUTPUT);
-  pinMode(latch_pin, OUTPUT);
-  pinMode(data_pin, OUTPUT);
-  DDRB = 0xFF;
+  DDRD = 0xFF;
 }
 
 void loop() {
-    led_Sequential(count);
-    //setLed(number_1[count]);
-    key1 = digitalRead(2);
-    key2 = digitalRead(3);
-    if(!key1 && count < 9)
-      count++;
-    else if(!key2 && count > 0)
-      count--;
+
+  key1 = digitalRead(2);
+  key2 = digitalRead(3);
+  led_Sequential(charecter[count]);
+    
+  if(!key1 && count < 9)
+    count++;
+  else if(!key2 && count > 0)
+    count--;
    
-    delay(100);
+  delay(100);
   
 }
 
-void led_Sequential(unsigned short count)
+void led_Sequential(unsigned short led)
 {
-    PORTB = 0x00; 
-    for(unsigned short i = 0; i < 8; i++)
+    PORTD = 0x00; //latch off
+    for(unsigned short i = 0; i < 8; ++i)
     {
-      number[count][7-i] ? PORTB = 0x08 : PORTB = 0x00;
-      PORTB = 0x20;
-      PORTB = 0x00;
+      (led >> (7-i)) & 0x01 ? PORTD = 0x10 : PORTD = 0x00; // data 0b0001 0000
+      PORTD = 0x04 | PORTD; //clock
+      PORTD = 0x00;
     }
-    PORTB = 0x04; 
-    delay(50);
-}
-
-void setLed(uint8_t state)
-{
-  digitalWrite(latch_pin,LOW);
-  for(int i = 0;i<8;i++){
-    digitalWrite(data_pin,(state>>(7-i)) &0x01);
-    digitalWrite(clk_pin,HIGH);
-    digitalWrite(clk_pin,LOW);
-    }
-  digitalWrite(latch_pin,HIGH);
+    PORTD = 0x08; //latch on 0b0000 1000
 }
